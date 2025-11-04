5f0a7735d323efaf24b46f53f821b16fe2c5ead6
Soeren Magnus Olesen <soeren@molesen.ch>
2025-11-04 12:23:59 +0100

    feat: Add new workflow for publishing Polymer packages and update project metadata
    Signed-off-by: Soeren Magnus Olesen <soeren@molesen.ch>

--- src/Polymer.Configuration/AssemblyInfo.cs
+++ src/Polymer.Configuration/AssemblyInfo.cs
@@ -1,3 +1,6 @@
 using System.Runtime.CompilerServices;
 
+[assembly: InternalsVisibleTo("Polymer.Cli")]
+[assembly: InternalsVisibleTo("Polymer.Codegen.Protobuf")]
+[assembly: InternalsVisibleTo("Polymer.Codegen.Protobuf.Generator")]
 [assembly: InternalsVisibleTo("Polymer.Tests")]
