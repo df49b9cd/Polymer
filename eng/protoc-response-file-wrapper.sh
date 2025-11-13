876c5eb8cd32b8babac305a6bd632af25763fa66
Soeren Magnus Olesen <soeren@molesen.ch>
2025-11-13 14:18:32 +0100

    feat: Enhance response file parsing in protoc wrapper with support for Python and Perl
    Signed-off-by: Soeren Magnus Olesen <soeren@molesen.ch>

--- eng/protoc-response-file-wrapper.sh
+++ eng/protoc-response-file-wrapper.sh
@@ -18,6 +18,53 @@ fi
 
 declare -a INVOCATION_ARGS=()
 
+parse_rsp_tokens() {
+  local rsp_file="$1"
+  if command -v python3 >/dev/null 2>&1; then
+    python3 - <<'PY' "${rsp_file}"
+import pathlib
+import shlex
+import sys
+
+path = pathlib.Path(sys.argv[1])
+data = path.read_text()
+lexer = shlex.shlex(data, posix=True)
+lexer.whitespace_split = True
+lexer.commenters = ''
+for token in lexer:
+    print(token)
+PY
+    return
+  elif command -v python >/dev/null 2>&1; then
+    python - <<'PY' "${rsp_file}"
+import pathlib
+import shlex
+import sys
+
+path = pathlib.Path(sys.argv[1])
+data = path.read_text()
+lexer = shlex.shlex(data, posix=True)
+lexer.whitespace_split = True
+lexer.commenters = ''
+for token in lexer:
+    print(token)
+PY
+    return
+  elif command -v perl >/dev/null 2>&1; then
+    perl -MText::ParseWords -e '
+      local $/;
+      my $content = <>;
+      foreach my $word (shellwords($content)) {
+        print "$word\n";
+      }
+    ' "${rsp_file}"
+    return
+  fi
+
+  echo "[protoc-wrapper] Unable to expand response file '${rsp_file}' because python3, python, and perl are not available." >&2
+  exit 92
+}
+
 expand_rsp() {
   local rsp_file="$1"
   if [[ ! -f "${rsp_file}" ]]; then
@@ -25,13 +72,7 @@ expand_rsp() {
     return
   fi
 
-  mapfile -t _tokens < <(perl -MText::ParseWords -e '
-    local $/;
-    my $content = <>;
-    foreach my $word (shellwords($content)) {
-      print "$word\n";
-    }
-  ' "${rsp_file}")
+  mapfile -t _tokens < <(parse_rsp_tokens "${rsp_file}")
 
   for token in "${_tokens[@]}"; do
     process_arg "${token}"
